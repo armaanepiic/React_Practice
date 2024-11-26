70788b687633d909e4a05eb9655d103bf51cb235
Arman Hossain <armaanepiicfb17@gmail.com>
2024-11-26 11:01:48 +0600

    I try by myself

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+function List() {
+  return (
+    <div className="List">
+      <p>Food list</p>
+      <ul>
+        <li>Mango</li>
+        <li>Apple</li>
+        <li>Orange</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
