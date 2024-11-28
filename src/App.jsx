1ca0b09240666b5e9341a4e4b88c31bfe28f2925
Arman Hossain <armaanepiicfb17@gmail.com>
2024-11-28 09:15:04 +0600

    react 2nd day

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import "./App.css";
 
-import "./App.css"
 export default function App() {
+
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+    },
+    {
+      id: 3,
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+    },
+    {
+      id: 4,
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Welcome Students!</h1>
-    </div>
-  )
+    <ul>
+      {people.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
 }
 
 
@@ -18,8 +49,8 @@ export default function App() {
 
 
 
-/*
 
+/*
 * A component must fulfill 3 criteria
   1) Component must be a function
   2) A component must return "something"
